ca3ecbf44546d2b3af3dd706135dbbeb59cc477b
James Taylor <james.taylor@elder-studios.co.uk>
2017-10-11 16:54:11 +0100

    seeders for db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,88 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Catrin Morgan',
+            'comment' => 'Nice use of contrast in this colour palette.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Cletus Spalding',
+            'comment' => 'It\'s splendid not just sleek!',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Grigor Martel',
+            'comment' => 'This is alluring work!',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Finnegan Draper',
+            'comment' => 'Super thought out! Leading the way, mate.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Zac Quincy',
+            'comment' => 'My 49 year old daughter rates this shot very splendid :)',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Grigor Martel',
+            'comment' => 'Such shot, many button, so simple.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Terrence Sutherland',
+            'comment' => 'Looks revolutionary and admirable, friend.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Terrence Sutherland',
+            'comment' => 'I want to learn this kind of shot! Teach me.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Zac Quincy',
+            'comment' => 'Incredible work you have here.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Grigor Martel',
+            'comment' => 'Engaging. It keeps your mind occupied while you wait.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Katharine McKenzie',
+            'comment' => 'White. This is new school.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('guest_book_entries')->insert([
+            'user' => 'Zac Quincy',
+            'comment' => 'I approve your shot.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
--- readme.md
+++ readme.md
@@ -12,11 +12,15 @@
 - ```php vendor/bin/homestead make``` generates a homestead.yml file which will be used to generate the virtual machine
 - ```vagrant up``` create and run the virtual machine
 
+- Copy the ```.env.example``` file and create a new file called ```.env```
+- Run ```php artisan key:generate``` to generate an application key which is used for encryption
+
 ## Database Migration
 
 - ```vagrant ssh``` to ssh onto the vagrant box
 - ```cd code``` navigate to the project code
 - ```php artisan migrate``` run the migrations that generate the database tables
+- ```php artisan db:seed``` to run the database seeder which will populate the table with dummy data
 - ```exit``` to leave the virtual machine terminal
 
 ## Access the site
